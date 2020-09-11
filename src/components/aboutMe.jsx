421b9ef91b9afc90d31e24c036d7a7b4b5b16661
Tapan Patel <tapan@pop-os.localdomain>
2020-09-11 19:18:20 +0530

    fixed header

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -1,21 +1,31 @@
 import React from 'react'
-import { Image,Heading } from 'rebass'
 
 import './aboutMe.scss'
 
-const AboutMe = ({to, color, value}) =>{
+import MyButton from './button'
+
+const AboutMe = () =>{
     return(
         <div className="aboutMe">
-            <div>
-                <Image/>
+            <h1>About Me</h1>
+            <div className="profile-img-container">
+                <img className="profile-img" src="./profile.png" alt="Tapan patel" width="100"/>
             </div>
-            <div>
-                <Heading>About Me</Heading>
-                <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
-                <table>
-                    
-                </table>
+            <div className="about-Info">
+                <div className="brief-about">
+                   <p> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam sapiente, error ipsam asperiores nam modi quas, esse exercitationem laboriosam tempora quidem culpa fugiat temporibus ipsum fuga inventore architecto ad iste.</p>
                 </div>
+                <table>
+                    <tr>
+                        <td className="answer">
+                            <MyButton value={"Github"} color={"yellow"} />
+                            <MyButton value={"Linkdin"} color={"purple"} />
+                            <MyButton value={"Twitter"} color={"green"} />
+                            <MyButton value={"Email"} color={"black"} />
+                        </td>
+                    </tr>
+                </table>
+            </div>
         </div>
     )
 }
--- src/components/mainSection.jsx
+++ src/components/mainSection.jsx
@@ -2,10 +2,10 @@ import React, {useRef} from 'react'
 
 import './mainSection.scss'
 
-// import AboutMe from './aboutMe'
+import AboutMe from './aboutMe'
 import Skills from './skill'
 import Projects from './project'
-import { Link } from 'gatsby'
+
 
 const toScroll = (myref) => {
     window.scrollTo({ behavior: 'smooth', top: myref.current.offsetTop })
@@ -15,8 +15,9 @@ const MainSection = ()=>{
     return(
         <div className="mainSection">
         <div className="InfoContainer">
-            <div ><Skills/></div>
-            <div ><Projects id="projects"/></div>
+            <AboutMe />
+            <Skills />
+            <Projects />
         </div>
         </div>
     );
