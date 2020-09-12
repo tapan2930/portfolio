3040c2ab732adc4b16935fea4f33739ff6ae7f68
Tapan Patel <tapan@pop-os.localdomain>
2020-09-12 23:16:21 +0530

    added experience section

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -17,17 +17,13 @@ const AboutMe = () =>{
                 <div className="brief-about">
                    <p>In a fullstack developer, living in India. I like to keep things simple and scalable. I always admire great user interface and try to make one. Connect with me on: </p>
                 </div>
-                <table>
-                    <tr>
-                        <td className="answer">
+               <div className="connect-cnt">
                             <MyButton to={"https://github.com/tapan2930"} value = {"Github"}  color={"#24292d"} icon={faGithub}/>
                             <MyButton to={"https://www.linkedin.com/in/tapan-patel-7567511a5/"} value = {"Linkdin"}  color={"#0077b4"} icon={faLinkedinIn}/>
                             <MyButton to={"https://www.instagram.com/tapan__p/"} value = {"Instagram"}  color={"#cf0076"} icon={faInstagram}/>
                             <MyButton to={"https://twitter.com/tapan2930"} value={"Twitter"} color={"#1da1f3"} icon={faTwitter} />
                             <MyButton to={"mailto:tapan2930@gmail.com"} value={"Email"} color={"#24292d"} icon={faEnvelopeOpen} />
-                        </td>
-                    </tr>
-                </table>
+                            </div>
             </div>
         </div>
     )
