b466289cdcf339e8fc7cb4e546ac82bcf0ddc44f
Tapan Patel <tapan@pop-os.localdomain>
2020-09-13 14:23:25 +0530

    fixed enlargement of icons

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { faEnvelopeOpen } from '@fortawesome/free-solid-svg-icons'
+// import { faEnvelopeOpen } from '@fortawesome/free-solid-svg-icons'
 import { faInstagram, faLinkedinIn, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 import './aboutMe.scss'
