028f054eac0a58ec7fcece30c71efcac0fbff0b4
Tapan Patel <tapan@pop-os.localdomain>
2020-09-13 14:41:26 +0530

    fixed package.json

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -9,7 +9,7 @@ const MyButton = ({to, color, value, icon}) =>{
     return(
         <div className="myButton">
             <div className={`linkBtn`} style={{backgroundColor:`${color.concat("50")}` ,  border:`1.5px solid ${color}`}} >
-                <FontAwesomeIcon style={{color:`${color}`, fontSize:"1.1rem"}} icon={icon} />
+                <FontAwesomeIcon style={{color:`${color}`, fontSize:"1.1rem", width:"20px", height:"20px"}} icon={icon} />
                 <Link to = {to} className="link" style={{color:`${color}`, fontWeight:"400", fontSize:"1.2rem"}} target="_blank" >{value}</Link>
             </div>  
         </div>
